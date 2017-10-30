8078c8caca719f672eaeb6a4bebf282f963cc0d0
César Gtz <cesareomargtz@gmail.com>
2017-10-30 13:55:15 -0700

    field tons_reception add

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Purchase Contract Type',
+    'version': '10.0',
+    'author': 'Qx Unit de Mexico SA de CV',
+    'website': 'http://www.qxunit.com.mx',
+    'depends': ['purchase', 'l10n_mx_invoice_amount_to_text','product'],
+    'data': [
+        'views/axc_contract.xml',
+        'views/purchase_contract_type.xml',
+        'views/purchase_order_line.xml',
+    ]
+}
---
+++ models/purchase_contract_type.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+import amount_to_text_es_MX
+
+
+class PurchaseContractType(models.Model):
+    _inherit = 'purchase.order'
+
+    is_signed = fields.Boolean()
+
+    auxiliary_contract = fields.Many2one('purchase.order', readonly=True)
+
+    contract_type = fields.Selection([
+        ('axc', 'AxC'),
+        ('pf', 'Precio Fijo'),
+        ('pm', 'Precio Minimo'),
+        ('pd', 'Precio Despues'),
+        ('pb', 'Precio Base'),
+	('sv', 'Servicios'),
+        ('surplus', 'Excedente'),
+        ('na', 'No aplica'),
+    ], default='na', required=True)
+
+    tons = fields.Float(compute='_compute_tons', store=False)
+    tons_text = fields.Text(compute='_compute_tons_text', store=False)
+    product = fields.Many2one('product.product', compute='_compute_product', store=False)
+
+    tons_reception = fields.Float(compute='_compute_tons_reception', store=False)
+
+    @api.one
+    def _compute_tons_reception(self):
+        available = 0
+        for line in self.env['stock.picking'].search([('group_id.name', '=', self.name), ('state', '=', 'done')]):
+            if "/IN/" in line['name']:
+                available = available + line['tons']
+            if "/OUT/" in line['name']:
+                available = available - line['tons']
+        self.tons_reception = available
+
+    @api.one
+    @api.depends('order_line')
+    def _compute_tons(self):
+        self.tons = 0
+        for line in self.order_line:
+            self.tons = line.product_qty
+            break
+
+    @api.one
+    @api.depends('tons')
+    def _compute_tons_text(self):
+        self.tons_text = amount_to_text_es_MX.get_amount_to_text(self, self.tons, 'es_MX', 'MX')
+
+    @api.one
+    @api.depends('order_line')
+    def _compute_product(self):
+        product = False
+        for line in self.order_line:
+            product = line.product_id
+            break
+        self.product = product
